7109afe9600fdc276c4a7210de76d6ae84765d3d
elvkikk <elviskika19@outlook.com>
2026-06-16 14:01:51 +0200

    Styled and fixed the render component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Layout } from "./components/Layout";
+import { InventoryProvider } from "./context/InventoryProvider";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { RoomPage } from "./pages/RoomPage";
+import { Victory } from "./pages/Victory";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <InventoryProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms/:roomPath" element={<RoomPage />} />
+            <Route path="/victory" element={<Victory />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </InventoryProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
